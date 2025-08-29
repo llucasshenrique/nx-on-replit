663aff4029d3a878149a3c65eab677bdfbff7fb9
Lucas Henrique <llucasshenrique@outlook.com>
2025-08-29 02:12:31 -0300

    feat: adicionar logging ao método getData no AppService

--- apps/server/src/app/app.service.ts
+++ apps/server/src/app/app.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { format } from 'date-fns';
 
 @Injectable()
 export class AppService {
+  private readonly logger: Logger = new Logger(AppService.name);
   getData(): { message: string } {
-    return { message: `Hello from the backend! At ${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}` };
+    this.logger.log('Fetching data...');
+    return {
+      message: `Hello from the backend! At ${format(
+        new Date(),
+        'yyyy-MM-dd HH:mm:ss'
+      )}`,
+    };
   }
 }
