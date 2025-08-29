ca21df827115e26c17b7883a48b9e4299cef23ba
Lucas Henrique <llucasshenrique@outlook.com>
2025-08-29 00:59:40 -0300

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/oaceliS4zY

---
+++ apps/client/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@nx-on-replit/client" />
+    </div>
+  );
+}
+
+export default App;
